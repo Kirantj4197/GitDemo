e84b187f7e9316946463135311b42c660845f1b3
Kirantj4197 <kirantj4197@gmail.com>
2022-06-25 12:08:52 +0530

    first commit

---
+++ src/test/java/features/Login.feature
@@ -0,0 +1,45 @@
+#Author: your.email@your.domain.com
+#Keywords Summary :
+#Feature: List of scenarios.
+#Scenario: Business rule through list of steps with arguments.
+#Given: Some precondition step
+#When: Some key actions
+#Then: To observe outcomes or validation
+#And,But: To enumerate more Given,When,Then steps
+#Scenario Outline: List of steps for data-driven as an Examples and <placeholder>
+#Examples: Container for s table
+#Background: List of steps run before each of the scenarios
+#""" (Doc Strings)
+#| (Data Tables)
+#@ (Tags/Labels):To group Scenarios
+#<> (placeholder)
+#""
+## (Comments)
+#Sample Feature Definition Template
+
+Feature: Application Login
+ 
+  Scenario: Landing on HomePage
+    Given User is on Landing Page
+    When user login to the application with "abc" and password "456"
+    Then Home Page is populated
+    And  links to all courses are displayed "true"
+    
+    
+    Scenario: Mobile Login
+    Given user launches the application
+    When user login to the application with "tjkr" and password "123456"
+    Then Home Page is populated
+    And  links to all courses are displayed "false"
+    
+    Scenario Outline: parameters explained
+    Given  User is on Page
+    When logins with <username> and <password>
+    Then links to all courses are validated
+    
+Examples:    
+|username |password |
+|abcd     |123456   |
+|tjkr     |1567890  |
+
+
---
+++ src/test/java/stepdefinitions/StepDefinition.java
@@ -0,0 +1,65 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinition {
+
+	
+
+    @Given("^User is on Landing Page$")
+    public void user_is_on_landing_page() throws Throwable {
+
+    System.out.println("sce  1  step 1");
+    }
+
+
+    @When("^user login to the application with \"([^\"]*)\" and password \"([^\"]*)\"$")
+    public void user_login_to_the_application_with_something_and_password_something(String strArg1, String strArg2) throws Throwable {
+   
+    	System.out.println(strArg1);
+    	System.out.println(strArg2);
+    }
+
+
+    @Then("^Home Page is populated$")
+    public void home_page_is_populated() throws Throwable {
+    	System.out.println("common  step 3");
+       
+    }
+
+    @And("^links to all courses are displayed \"([^\"]*)\"$")
+    public void links_to_all_courses_are_displayed_something(String strArg3) throws Throwable {
+       System.out.println(strArg3);
+    }
+
+
+    @Given("^user launches the application$")
+    public void user_launches_the_application() throws Throwable {
+       System.out.println("sce 2 step 1");
+    }
+    
+
+    @Given("^User is on Page$")
+    public void user_is_on_page() throws Throwable {
+        
+    }
+
+    @When("^logins with (.+) and (.+)$")
+    public void logins_with_and(String username, String password) throws Throwable {
+       
+    }
+
+    @Then("^links to all courses are validated$")
+    public void links_to_all_courses_are_validated() throws Throwable {
+     
+    }
+
+  
+
+    
+    
+    
+}
